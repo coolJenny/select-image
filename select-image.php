1f2efbc1aa7e805a511e43e4dc6d2627168e4cfe
coolJenny <coolJenny009g@gmail.com>
2018-03-13 08:25:01 -0400

    first commit

---
+++ select-image.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name:  Select Image
+Plugin URI:   https://cool.wordpress.org/plugins/select-image/
+Description:  Select images from Amazon
+Version:      3.14
+Author:       Cool
+Author URI:   https://cool.wordpress.org/
+License:      GPL2
+License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain:  select-image
+Domain Path:  /languages/
+*/
